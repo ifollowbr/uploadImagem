2e75a286b43fc6b3954fdb7c29f5ff8f95ffbd5d
ifollowbr <tech@ifollow.com.br>
2024-08-27 10:29:24 -0300

    multi arquivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import api from './services/api';
+import {useRef} from 'react';
 
 export default function App(){
   const [base64Imagem,setBase64Imagem] = useState('')
   const [url,setUrl] = useState('')
+  const [files, setFiles] = useState([]);
 
   //SELECIONA ARQUIVO & CHAMA FUNÇÃO CONVERTER BASE64
   async function imageChange(e){
@@ -44,6 +46,57 @@ export default function App(){
     })
   }
 
+  function uploadMultiplos(){
+      const formData = new FormData();
+      for (let i = 0; i < files.length; i++) {
+          formData.append("files", files[i]);  // Adiciona cada arquivo ao `FormData`
+      }
+
+      api.post('/imagem_material', formData,
+        {
+          headers: {
+              "Content-Type": "multipart/form-data"
+          } 
+        })
+      .then((result)=>{
+        let response = result.data.response.url
+        setUrl(response)
+      })
+      .catch((err)=>{
+        console.log(err)
+    })
+  }
+
+
+  //BACKEND MULTIPLOS ARQUIVOS
+  // const files = request.files.files;
+
+  // //VERIFICAÇÃO DE LISTA
+  // if (!Array.isArray(files)) {
+  //   return response.status(400).json({ message: 'Nenhum arquivo enviado!' });
+  // }
+
+  // const uploadedFiles = [];
+
+  // files.forEach((file, index) => {
+  //   // Aqui você deve decodificar e salvar o arquivo
+  //   const buffer = Buffer.from(file.data, 'base64'); // Supondo que o arquivo seja enviado como base64
+  //   const filePath = path.join(__dirname, '../../uploads', `${Date.now()}-${index}-${file.name}`);
+    
+  //   fs.writeFileSync(filePath, buffer);
+
+  //   uploadedFiles.push({
+  //     originalname: file.name,
+  //     path: filePath,
+  //     size: buffer.length,
+  //   });
+  // });
+
+  // response.json({
+  //   message: 'Arquivos salvos com sucesso!',
+  //   files: uploadedFiles
+  // });
+
   return(
     <div>
         <h1>SELECIONE UMA IMAGEM</h1>
@@ -53,6 +106,22 @@ export default function App(){
         <br/><br/>
         <button onClick={uploadImage}>SALVAR</button>
         <br/><br/>
+
+        <input
+          type='file'
+          id="file"
+          multiple={true}
+          accept='image/png, image/jpg, image/jpeg'
+          ref={inputRef}
+          onChange={async (e) => {
+              const files = Array.from(e.target.files);
+              setFiles((prevFiles) => [
+                  ...prevFiles,
+                  ...files
+              ]);
+          }}
+          />
+          <button onClick={()=> uploadMultiplos()}>Confirmar</button>
     </div>
   )
 }
